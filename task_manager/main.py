67343345fe7d472ae12b8e9f3f68562b844afe31
DK <don@promptnow.com>
2025-03-16 15:36:43 +0700

    Refactor code

--- task_manager/main.py
+++ task_manager/main.py
@@ -1,9 +1,10 @@
+from datetime import datetime
 from json import JSONDecodeError
 from manager import TaskManager
 from logger import Log
 
 def main():
-    logPrefix = "[-main-]"
+    log_prefix = "[-main-]"
     while True:
         print("\nDK's Task Manager ::")
         print("1. Add Task")
@@ -15,41 +16,45 @@ def main():
         choice = input("Choose an option: ")
         
         if choice == "1":
+            Log.info(log_prefix, "Choice 1 selected")
+            # prompt for input
+            title = input("Enter task title: ")
+            description = input("Enter task description: ")
+            due_date = input("Enter due date (YYYY-MM-DD): ")
             try:
                 # format time
+                datetime.strptime(due_date, "%Y-%m-%d")
                 # f() create a task
-                Log.info(logPrefix, "Choice 1 selected")
-                title = input("Enter task title: ")
-                description = input("Enter task description: ")
-                due_date = input("Enter due date (YYYY-MM-DD): ")
+                TaskManager.add_task(title, description, due_date)
+                
             except ValueError:
-                Log.error(logPrefix, "Invalid date format.")
+                Log.error(log_prefix, "Invalid date format.")
         elif choice == "2":
+            Log.info(log_prefix, "Choice 2 selected")
             try:
-                Log.info(logPrefix, "Choice 2 selected")
                 print("\n[- View All Tasks -]")
                 TaskManager.view_tasks()
             except JSONDecodeError:
-                Log.error(logPrefix, "JSON parsing failed or file is empty.")
+                Log.error(log_prefix, "JSON parsing failed or file is empty.")
         elif choice == "3":
+            Log.info(log_prefix, "Choice 3 selected")
             # prompt for task id
             # f() mark task as complete
-            Log.info(logPrefix, "Choice 3 selected")
         elif choice == "4":
+            Log.info(log_prefix, "Choice 4 selected")
             # prompt for task id
             # f() delete task
-            Log.info(logPrefix, "Choice 4 selected")
         elif choice == "5":
+            Log.info(log_prefix, "Choice 5 selected")
             # prompt for keyword
             # f() search for tasks
-            Log.info(logPrefix, "Choice 5 selected")
             print()
         elif choice == "6":
-            Log.info(logPrefix, "Choice 6 selected")
-            Log.info(logPrefix, "Exiting DK's Task Manager...")
+            Log.info(log_prefix, "Choice 6 selected")
+            Log.info(log_prefix, "Exiting DK's Task Manager...")
             break
         else:
-            Log.error(logPrefix, "Invalid input.")
+            Log.error(log_prefix, "Invalid input.")
             print("Invalid choice! Please enter a number between 1-6.")
 
 if __name__ == "__main__":
